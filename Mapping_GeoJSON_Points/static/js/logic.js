edac76b5c0f5f4a90a1a6a65f0477583e650c6da
Alan Goldberg <alan.goldberg@yahoo.com>
2021-05-23 17:56:05 -0400

    add multiple layers (street/dark) to the map

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([30, 30], 2);
+//let map = L.map("mapid").setView([30, 30], 2);
 
 /*  prior options commented
     // Add GeoJSON FeatureCollection data for SFO Airport.
@@ -85,8 +85,10 @@ L.geoJson(sanFranAirport, {
     mapbox/navigation-day-v1
     mapbox/navigation-night-v1
 */
+
+// add tileLayers for streets and dark views
 let streets = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -95,7 +97,33 @@ let streets = L.tileLayer(
   }
 );
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//streets.addTo(map);
+
+let dark = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark,
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map("mapid", {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets],
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL (always load larger, slower data after display)
 let airportData =
